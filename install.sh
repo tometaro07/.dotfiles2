b518b7dd66b9abeae1b786dae2940a26eedd0345
tometaro <tomasrocha01@hotmail.com>
2025-07-15 20:46:49 +0100

    clean install file

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo dnf install pip
 pip install pywal
 
 echo "Installing SwayVM group addapted"
-sudo dnf install NetworkManager-l2tp-gnome NetworkManager-libreswan-gnome NetworkManager-openconnect-gnome NetworkManager-openvpn-gnome NetworkManager-pptp-gnome NetworkManager-sstp-gnome NetworkManager-vpnc-gnome nautilus blueman bolt fprintd-pam gnome-keyring-pam gnome-themes-extra gvfs gvfs-smb imv kanshi lxqt-policykit mpv pavucontrol pinentry-gnome3 playerctl pulseaudio-utils  sddm qt6-qtvirtualkeyboard qt6-qtmultimedia system-config-printer wev wl-clipboard wlr-randr xarchiver xdg-desktop-portal-gtk firefox
+sudo dnf install nautilus blueman bolt fprintd-pam gnome-keyring-pam gnome-themes-extra gvfs gvfs-smb imv kanshi lxqt-policykit mpv pavucontrol pinentry-gnome3 playerctl pulseaudio-utils  sddm qt6-qtvirtualkeyboard qt6-qtmultimedia system-config-printer wev wl-clipboard wlr-randr xarchiver xdg-desktop-portal-gtk firefox
 
 echo "Install Ghostty and Starship"
 sudo dnf copr enable pgdev/ghostty
@@ -27,12 +27,9 @@ sudo dnf remove kitty
 
 echo "Install Tools"
 
-sudo dnf copr enable pennbauman/ports
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-sudo dnf install micro qalc fzf lf vlc inkscape flatpak code texlive-scheme-full texlive-chktex okular libreoffice shotwell gimp stow cabextract xorg-x11-font-utils
-
+sudo dnf install micro qalc fzf vlc inkscape flatpak texlive-scheme-full texlive-chktex okular libreoffice shotwell gimp stow cabextract xorg-x11-font-utils
 sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
 echo "Install social"
@@ -46,7 +43,7 @@ flatpak install flathub com.spotify.Client
 
 echo "Install Others"
 
-sudo dnf install fastfetch gnome-system-monitor gnome-calendar
+sudo dnf install gnome-system-monitor gnome-calendar
 
 echo "Setup config files"
 cd ~/.dotfiles2
@@ -54,9 +51,6 @@ stow hyprland/
 stow terminal/
 
 echo "Setup sddm"
-systemctl enable sddm.service
-systemctl set-default graphical.target
-
 cd ~/.sddm-layouts/SilentSDDM/
 sudo mkdir -p /usr/share/sddm/themes/silent
 sudo cp -rf . /usr/share/sddm/themes/silent/
@@ -91,3 +85,10 @@ apply_theme () {
 }
 
 apply_theme
+
+systemctl enable sddm.service
+systemctl set-default graphical.target
+
+echo "" >> ~/.bashrc
+echo "# Starship" >> ~/.bashrc
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
